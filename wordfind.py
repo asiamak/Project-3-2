26b877e194cb68544ac2ca1ae7572a4b6c9caeb1
asiamak <59885210+asiamak@users.noreply.github.com>
2020-12-05 10:21:53 -0500

    Add files via upload
    Implemented main function that prompts users for input

--- wordfind.py
+++ wordfind.py
@@ -2,16 +2,21 @@
 def wordFind(inputfile,found): 
   with open(inputfile,'r') as file: #open file to be read
       linenum=1;
-      foundnum=1;
+      foundnum=0;
       # read each line     
       for line in file: 
           # read each word       
           for word in line.split(): #for words in line
               # display the result            
               if(word == found): #match the word with user entered word.
-                print(word," was found on line ",l)
+                print(word," was found on line ",linenum)
                 foundnum+=1
           linenum+=1 
-      if(foundnum > 0):
-         print(word," was not found")
+      if(foundnum == 0):
+         print(found," was not found")
 
+#main 
+if __name__ == "__main__":
+  inputfile = str(input("Enter File Name: "))
+  found = str(input("Enter name to be Searched in File: "))
+  wordFind(inputfile,found)#call function
